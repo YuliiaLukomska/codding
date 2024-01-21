0fefb530b5ce326ee271487ef1244dbab33f6786
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-01-21 16:42:18 +0000

    Deploying to gh-pages from @ YuliiaLukomska/codding@ac73e05cc8a81311ba38a7160b1684fe50ce26b7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,2 @@
-import{S as i,i as c}from"./assets/vendor-46aac873.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&o(l)}).observe(document,{childList:!0,subtree:!0});function a(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerpolicy&&(r.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?r.credentials="include":t.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=a(t);fetch(t.href,r)}})();const u="/codding/assets/left-close-f1c537d4.svg",n={form:document.querySelector("form"),button:document.querySelector("button"),list:document.querySelector("ul")},p="41896397-c8b989416d0fb53fd1030eb96",d="https://pixabay.com/api/";n.form.addEventListener("submit",f);function f(s){s.preventDefault(),n.list.innerHTML='<span class="loader"></span>';const e=s.currentTarget,a=e.elements.image.value;m(a).then(o=>{n.list.innerHTML=g(o),new i(".gallery a",{captionsData:"alt",captionDelay:250}).refresh(),o.hits.length===0&&c.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FAFAFB",messageLineHeight:"24px",messageSize:"16px",position:"topRight",iconUrl:u,backgroundColor:"#EF4040",maxWidth:"350px",timeout:!1})}).catch(o=>console.log(o)).finally(()=>{e.reset()})}function m(s){const e=new URLSearchParams({key:p,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${d}?${e}`).then(a=>{if(a.ok)return a.json();throw new Error(a.statusText)})}function g(s){return s.hits.map(e=>`<li class="gallery-item">
-          <a class="gallery-link" href="${e.largeImageURL}"
-            ><img class="gallery-img" src="${e.webformatURL}" data-sourse="${e.largeImageURL}" alt="${e.tags}"
-          /></a>
-          <div class="description-wrapper">
-            <div class="value-wrapper">
-              <p class="label"><b>Likes</b></p>
-              <p class="value">${e.likes}</p>
-            </div>
-            <div class="value-wrapper">
-              <p class="label"><b>Views</b></p>
-              <p class="value">${e.views}</p>
-            </div>
-            <div class="value-wrapper">
-              <p class="label"><b>Comments</b></p>
-              <p class="value">${e.comments}</p>
-            </div>
-            <div class="value-wrapper">
-              <p class="label"><b>Downloads</b></p>
-              <p class="value">${e.downloads}</p>
-            </div>
-          </div>
-        </li>`).join("")}
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
 //# sourceMappingURL=commonHelpers.js.map
