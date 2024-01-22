0d7f4132e4ef5ce046b998f7b565934445a64fd4
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-01-22 18:33:57 +0200

    add function renderTasksfromLS

--- src/index.js
+++ src/index.js
@@ -30,3 +30,27 @@ function onFormSubmit(event) {
   localStorageAPI.saveTask(task);
   form.reset();
 }
+
+// створимо функцію, яку будемо викликати при кожному завантаженні сторінки
+// (при перезавантаженні сторінки відповідно теж). Ця функція буде брати з localStorage масив(він зберігається в ключі) і
+// по цьому масиву робити розмітку карток. Таким чином при перезав.сторінки картки не будуть зникати.
+
+function renderTaskfromLS() {
+  // отримуємо масив об'єктів зlocalStorage (обовязково треба розпарсити дані)
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  // перебираємо цей масив map-ом - map повертає масив такої ж довжини в якому кожен елемент буде тим, що поверне кол-бек функція.
+  // Перетворюємо масив в рядок за доп.join('');
+  const markup = tasks
+    .map(
+      obj => `<li class="task-list-item">
+      <button class="task-list-item-btn">Удалить</button>
+      <h3>${obj.taskName}</h3>
+      <p>${obj.taskText}</p>
+  </li>`
+    )
+    .join('');
+  refs.list.insertAdjacentHTML('beforeend', markup);
+}
+
+renderTaskfromLS();
